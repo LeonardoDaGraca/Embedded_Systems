074ea74734365afc5434d9174527a033e9704194
Leo <leonardo.dagraca@maine.edu>
2024-10-31 20:46:30 -0400

    HW5 commit

--- HW5/motor.c
+++ HW5/motor.c
@@ -1,37 +1,52 @@
-#include <signal.h>
+/*
+ * Leonardo DaGraca 
+ * HW5 : Analog Signals using a DC Motor
+ * 
+ * Program allows the user to do the following:
+ * make motor work in a binary way
+ * make motor work at varying speeds
+ * 
+ * All functionality will signify how fast the motor is moving
+ * by using LEDs
+ * Red LED = Low or no motor movement
+ * Yellow LED = moderate motor movement
+ * Green LED = high motor movement
+ */
+ 
 #include <stdio.h>
+#include <signal.h>
 #include <pigpio.h>
+#include <stdlib.h>
+#include "motor.h"
 
-const int outMotor = 21;
 
-volatile sig_atomic_t signal_received = 0;
-
-void sigint_handler(int signal)
-{
-	signal_received = signal;
+void sigint_handler(int signal) {
+    signal_received = signal;
 }
 
-int main()
-{
-	if (gpioInitialise() == PI_INIT_FAILED) {
-		printf("ERROR: Failed to initialize the GPIO interface.\n");
-	}
-	gpioSetMode(outMotor, PI_OUTPUT);
-	signal(SIGINT, sigint_handler);
-	printf("Press Ctrl-C to exit.\n");
-	
-	while (!signal_received) {
-		for (int i = 0; i < 256; i++) {
-			gpioPWM(outMotor, i);
-			time_sleep(0.01);		
-		}
-		time_sleep(0.1);
-	}
 
-	gpioSetMode(outMotor, PI_INPUT);
-	gpioTerminate();
-	printf("\n");
+int main() {
+    if (gpioInitialise() == PI_INIT_FAILED) {
+        printf("ERROR: Failed to initialize the GPIO interface.\n");
+    }
+    gpioSetMode(OUT_MOTOR, PI_OUTPUT);
+    gpioSetMode(OUT_LOW, PI_OUTPUT);
+    gpioSetMode(OUT_MODERATE, PI_OUTPUT);
+    gpioSetMode(OUT_HIGH, PI_OUTPUT);
+    
+    print_menu();
 
-	return 0;	
+    signal(SIGINT, sigint_handler);
+    while (!signal_received) {
+        handle_menu_input();
+    }
+    gpioSetMode(OUT_MOTOR, PI_INPUT);
+    gpioSetMode(OUT_LOW, PI_INPUT);
+    gpioSetMode(OUT_MODERATE, PI_INPUT);
+    gpioSetMode(OUT_HIGH, PI_INPUT);
+    gpioTerminate();
+
+    printf("\n");
+
+    return 0;
 }
-
--- HW5/motor.h
+++ HW5/motor.h
@@ -1,14 +1,30 @@
+/*
+ * Leonardo DaGraca 
+ * HW5 : Analog Signals using a DC Motor
+ * 
+ * Header file with utility functions to assist motor.c
+ */
+ 
 #ifndef MOTOR_H
 #define MOTOR_H
 
 #include <stdio.h>
 #include <pigpio.h>
 
-const int outMotor = 21;
+const int OUT_MOTOR = 21;
+const int OUT_LOW = 13;
+const int OUT_MODERATE = 19;
+const int OUT_HIGH = 26;
+
+volatile sig_atomic_t signal_received = 0;
 
 void binary_mode();
 void speed_mode();
 int handle_binary_input();
+void toggle_motor();
+void set_speed(int desired_speed);
+int led_out(int state);
+void parse_speed(int speed);
 
 void print_menu() {
     printf("\n");
@@ -21,27 +37,66 @@ void print_menu() {
 }
 
 void binary_mode() {
-    printf("\n");
-    printf("Binary Mode\n");
-    printf("-----------\n");
-    printf("Enter 1: Switch Motor on/off\n");
-    printf("Enter 2: Return to Main Menu\n");
-    printf("Enter CTRL-C: Exit Application\n");
-    handle_binary_input();
+    while (!signal_received) {
+        printf("\n");
+        printf("Binary Mode\n");
+        printf("-----------\n");
+        printf("Enter 1: Switch Motor on/off\n");
+        printf("Enter 2: Return to Main Menu\n");
+        printf("Enter CTRL-C: Exit Application\n");
+        
+	char input[10];
+	if (fgets(input, sizeof(input), stdin)) {
+	    if (signal_received) return;
+
+	    if (input[0] == '1') {
+	        toggle_motor();
+	    } else if (input[0] == '2') {
+                print_menu();
+		break;
+            } else {
+	        printf("Enter a valid selection\n");
+	    }
+	}
+    }
 }
 
 void speed_mode() {
-    printf("\n");
-    printf("Speed Mode\n");
-    printf("-----------\n");
-//    printf("Enter 1: Switch Motor on/off\n");
-//    printf("Enter 2: Return to Main Menu\n");
-    printf("Enter CTRL-C: Exit Application\n");
+    while (!signal_received) {
+	printf("\n");
+	printf("Select Speed Mode\n");
+	printf("-----------\n");
+	printf("Enter s <speed>: Set Motor Speed (0-255)\n");
+	printf("Enter 2: Return to Main Menu\n");
+	printf("Enter CTRL-C: Exit Application\n");
+	
+	char input[10];
+	if (fgets(input, sizeof(input), stdin)) {
+	    if (signal_received) return;
+
+	    if (input[0] == 's') {
+	        int desired_speed;
+		if (sscanf(input + 1, "%d", &desired_speed) == 1) {
+		    set_speed(desired_speed);
+		}
+		else {
+		    printf("Invalid speed input. Please enter an integer between 0 - 255\n");
+		}
+	    } else if (input[0] == '2') {
+                print_menu();
+		break;
+            } else {
+	        printf("Enter a valid selection\n");
+	    }
+	}
+    }
 }
 
 int handle_menu_input() {
     char input[10];
     if (fgets(input, sizeof(input), stdin)) {
+	if (signal_received) return 1;
+
         if (input[0] == '1') {
             //run binary mode
             binary_mode();
@@ -53,39 +108,88 @@ int handle_menu_input() {
         }
     }
 
-    return 0; // Continue running
+    return 0;
 }
 
-int handle_binary_input() {
-    char input[10];
-    if (fgets(input, sizeof(input), stdin)) {
-        if (input[0] == '1') {
-            if (gpioRead(outMotor) == PI_LOW) {
-                for (int i = 0; i < 256; i++) {
-                    gpioPWM(outMotor, i);
-                    time_sleep(0.01);
-                }
-                time_sleep(0.1);
-                gpioPWM(outMotor, 255);
-            }
-            else {
-                for (int i = 255; i >= 0; i--) {
-                    gpioPWM(outMotor, i);
-                    time_sleep(0.01);
-                }
-                time_sleep(0.1);
-                gpioPWM(outMotor, i);
-            }
-        } else if (input[0] == '2') {
-            //return to the menu
-            print_menu();
-        } else {
-            printf("Enter a valid selection");
-        }
+void toggle_motor() {
+    if (gpioRead(OUT_MOTOR) == PI_LOW) { 
+        //motor on 
+        int i; 
+        for (i = 0; i < 256; i++) {  
+            gpioPWM(OUT_MOTOR, i); 
+            time_sleep(0.01); 
+        } 
+            time_sleep(0.1); 
+            gpioPWM(OUT_MOTOR, 255); 
+	    parse_speed(255);
+            printf("Motor is ON\n"); 
+    } 
+    else { 
+	//motor off
+        int i; 
+        for (i = 255; i >= 0; i--) { 
+            gpioPWM(OUT_MOTOR, i); 
+            time_sleep(0.01); 
+        } 
+        time_sleep(0.1); 
+        gpioPWM(OUT_MOTOR, 0);
+	parse_speed(0);
+	printf("Motor is OFF\n"); 
     }
-
-    return 0; // Continue running
 }
 
+void set_speed(int desired_speed) {
+    //clamp speeds
+    if (desired_speed < 0) desired_speed = 0;
+    if (desired_speed > 255) desired_speed = 255;
+    
+    gpioWrite(OUT_MOTOR, PI_LOW);
+    int i;
+    for (i = 0; i <= desired_speed; i++) {
+	gpioPWM(OUT_MOTOR, i);
+	time_sleep(0.01);
+    }
+    time_sleep(0.1);
+    gpioPWM(OUT_MOTOR, desired_speed);
+    parse_speed(desired_speed);
+    
+    int percentage = (desired_speed * 100) / 255;
+    printf("Motor is running at %d%% speed\n", percentage);
+}
+
+int led_out(int state) {
+    gpioWrite(OUT_LOW, PI_LOW);
+    gpioWrite(OUT_MODERATE, PI_LOW);
+    gpioWrite(OUT_HIGH, PI_LOW);
+    
+    if (state == 0) {
+	//turn red led on
+	gpioWrite(OUT_LOW, PI_HIGH);
+	return 0;
+    }
+    if (state == 1) {
+	//turn yellow led on
+	gpioWrite(OUT_MODERATE, PI_HIGH);
+	return 0;
+    }
+    if (state == 2) {
+	//turn green led on
+	gpioWrite(OUT_HIGH, PI_HIGH);
+	return 0;
+    }
+    return 1;
+}
+
+void parse_speed(int speed) {
+    if (speed >= 0 && speed <= 65) {
+	led_out(0);
+    }
+    else if (speed > 65 && speed <= 165) {
+	led_out(1);
+    }
+    else if (speed > 165 && speed <= 255){
+	led_out(2);
+    }
+}
 
 #endif
