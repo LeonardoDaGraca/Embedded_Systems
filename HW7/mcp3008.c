97a518999d15185b23b1f0aa1af2013cff2ead9a
Leo <leonardo.dagraca@maine.edu>
2024-11-22 11:56:49 -0500

    update

--- HW7/mcp3008.c
+++ HW7/mcp3008.c
@@ -1,48 +1,94 @@
+/*
+ * Leonardo DaGraca 
+ * HW7 : A2D Conversion
+ * 
+ * Program allows the user to do the following:
+ * control brightness of LED with one potentiometer
+ * control flash speed of LED with another potentiometer
+ * 
+ */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pigpio.h>
+#include <pthread.h>
 
 #define RED_LED 21
+#define POT1_CHANNEL 0 //brightness channel
+#define POT2_CHANNEL 1 //flash channel
 
 /*
  gcc -pthread -o mcp3008 mcp3008.c -lpigpio -lrt
  mcp3008 [num_of_loops] [speed_of_transmission]
  defaults to 1000000 in both cases
 */
+volatile int flash_delay = 500;
+pthread_mutex_t delay_mutex;
+
+void *flash_thread(void *arg) {
+    while (1) {
+        pthread_mutex_lock(&delay_mutex);
+        int delay_ms = flash_delay;
+        pthread_mutex_unlock(&delay_mutex);
+
+        gpioWrite(RED_LED, PI_HIGH);
+        time_sleep(delay_ms / 1000.0);
+        gpioWrite(RED_LED, PI_LOW);
+        time_sleep(delay_ms / 1000.0);
+    }
+    return NULL;
+}
+
+int read_adc(int spi_handle, int channel) {
+    unsigned char buf[3];
+    buf[0] = 1;
+    buf[1] = (8 + channel) << 4;
+    buf[2] = 0;
+    spiXfer(spi_handle, buf, buf, 3);
+    return ((buf[1] & 3) << 8) | buf[2];
+}
+
 int main(int argc, char *argv[]) {
-    int i;
-    int h;
-    int v;
     int loops;
     int speed;
-    double start, diff, sps;
-    unsigned char buf[3];
+    int spi_handle;
+
     if (argc > 1) loops = atoi(argv[1]);
     else loops = 1000000;
+
     if (argc > 2) speed = atoi(argv[2]);
     else speed = 1000000;
-    if (gpioInitialise() < 0) return 1;
-    h = spiOpen(0, speed, 0);
-    if (h < 0) return 2;
-    start = time_time();
 
+    if (gpioInitialise() < 0) return 1;
     gpioSetMode(RED_LED, PI_OUTPUT);
 
-    for (i = 0; i < loops; i++) {
-        buf[0] = 1;
-        buf[1] = 128;
-        buf[2] = 0;
-        spiXfer(h, buf, buf, 3);
-        v = ((buf[1] & 3) << 8) | buf[2];
-        printf("%d\n", v);
-        int pwm = (int) ((1023 - v) / 1024.0 * 256);
-        gpioPWM(RED_LED, pwm);
+    spi_handle = spiOpen(0, speed, 0);
+    if (spi_handle < 0) return 2;
+
+    pthread_t flash_tid;
+    pthread_mutex_init(&delay_mutex, NULL);
+    pthread_create(&flash_tid, NULL, flash_thread, NULL);
+
+    for (int i = 0; i < loops; i++) {
+       int brightness = read_adc(spi_handle, POT1_CHANNEL);
+       int delay_value = read_adc(spi_handle, POT2_CHANNEL);
+
+       int pwm = (int)((1023 - brightness) / 1024.0 * 256.0);
+       gpioPWM(RED_LED, pwm);
+
+       pthread_mutex_lock(&delay_mutex);
+       flash_delay = (int)((delay_value / 1024.0) * 1000);
+       pthread_mutex_unlock(&delay_mutex);
+
+       time_sleep(0.1);
     }
-    diff = time_time() - start;
-    fprintf(stderr, "sps=%.1f @ %d bps (%d/%.1f)\n",
-            (double) loops / diff, speed, loops, diff);
-    spiClose(h);
+    pthread_cancel(flash_tid);
+    pthread_join(flash_tid, NULL);
+
+    pthread_mutex_destroy(&delay_mutex);
+    spiClose(spi_handle);
     gpioSetMode(RED_LED, PI_OUTPUT);
     gpioTerminate();
+
     return 0;
 }
